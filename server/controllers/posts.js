22514ad86bf305729f672f35ceabb29da592d0a0
gurseerat17 <57999895+gurseerat17@users.noreply.github.com>
2020-12-30 15:19:12 +0530

    Add files via upload

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,28 +1,80 @@
-import Post from '../models/post.js';
+import express from 'express';
+import mongoose from 'mongoose';
 
-export  const getPosts = async(req, res) => {
-    try{
-        const posts = await Post.find();
-        res.status(200).json(posts);
-    }
-    catch(error){
-        res.status(404).json({message: error.message});
-        console.log(error);
+import PostMessage from '../models/postMessage.js';
+
+const router = express.Router();
+
+export const getPosts = async (req, res) => { 
+    try {
+        const postMessages = await PostMessage.find();
+                
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
     }
 }
 
-export const createPost = (req, res) => {
-    const post = req.body;
-    const newPost = new Post(post);
-    try{
-        newPost.save();
-        res.status(201).json(newPost);
-    }
-    catch(error){
-        res.status(409).json({message: error.message});
+export const getPost = async (req, res) => { 
+    const { id } = req.params;
+
+    try {
+        const post = await PostMessage.findById(id);
+        
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
     }
 }
 
-export const deletePost = (req, res) => {
+export const createPost = async (req, res) => {
+    const { title, post_content, selectedFile, author, tags } = req.body;
+
+    const newPostMessage = new PostMessage({ title, post_content, selectedFile, author, tags })
+
+    try {
+        await newPostMessage.save();
+
+        res.status(201).json(newPostMessage );
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+}
+
+export const updatePost = async (req, res) => {
+    const { id } = req.params;
+    const { title, post_content, author, selectedFile, tags } = req.body;
     
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    const updatedPost = { author, title, post_content, tags, selectedFile, _id: id };
+
+    await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+
+    res.json(updatedPost);
 }
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.json({ message: "Post deleted successfully." });
+}
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+    
+    const post = await PostMessage.findById(id);
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+    
+    res.json(updatedPost);
+}
+
+
+export default router;
